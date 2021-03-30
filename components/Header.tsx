334f53e9677773f766793c53ba6a78aec75db832
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-03-30 14:44:31 -0500

    keep it organized

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import { ReactElement } from 'react';
 import { ActiveLink } from './ActiveLink';
+import style from 'styles/Header.module.css';
 
 export default function Header():ReactElement {
   return (
-    <header>
+    <header className={style.header}>
       <h3><Link href='/'>John Barhorst</Link></h3>
-      <nav>
+      <nav className={style.nav}>
         <ActiveLink href='/'>Home</ActiveLink>
         <ActiveLink href='/projects'>Projects</ActiveLink>
         <ActiveLink href='/about'>About</ActiveLink>
