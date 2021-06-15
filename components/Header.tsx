2b56dcb2da16e3938d75e2c06821e4acd60bcdb2
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-06-15 17:39:53 -0500

    animated hamburger

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,44 @@ import Link from 'next/link';
 import { ReactElement } from 'react';
 import { ActiveLink } from './ActiveLink';
 import style from 'styles/Header.module.css';
-import { AnimateSharedLayout } from 'framer-motion';
+import { AnimateSharedLayout, motion } from 'framer-motion';
+import useToggle from 'hooks/useToggle';
+
+const topbarVariants = {
+  open: {
+    y: 8,
+    rotate: 45
+  },
+  closed: {
+    y: 0,
+    rotate: 0 
+  }
+};
+
+const midbarVariants = {
+  open: {
+    opacity: 0,
+    transition: {
+      duration: 0
+    }
+  },
+  closed: {
+    opacity: 1
+  }
+};
+
+const bottombarVariants = {
+  open: {
+    y: -8,
+    rotate: -45
+  },
+  closed: {
+    y: 0
+  }
+};
 
 export default function Header():ReactElement {
+  const { isToggled: isOpen, toggle: toggleOpen, setToggle } = useToggle(false);
   return (
     <header className={style.header}>
       <h3><Link href='/'>John Barhorst</Link></h3>
@@ -15,6 +50,27 @@ export default function Header():ReactElement {
           <ActiveLink href='/about' text='About' />
           <ActiveLink href='/contact' text='Get In Touch' />
         </nav>
+        <motion.button 
+          type='button' 
+          className={style.hamburger}
+          onClick={toggleOpen}
+        >
+          <motion.span 
+            className={style.bar} 
+            variants={topbarVariants}
+            animate={isOpen ? 'open' : 'closed'}
+          ></motion.span>
+          <motion.span 
+            className={style.bar}
+            variants={midbarVariants}
+            animate={isOpen ? 'open' : 'closed'}
+          ></motion.span>
+          <motion.span 
+            className={style.bar}
+            variants={bottombarVariants}
+            animate={isOpen ? 'open' : 'closed'}
+          ></motion.span>
+        </motion.button>
       </AnimateSharedLayout>
     </header>
   );
