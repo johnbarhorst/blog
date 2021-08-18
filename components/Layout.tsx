14a82dd24662134296bd91a939cc78f49ead8a88
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-08-17 20:45:01 -0500

    Iphone landscape coverage

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,6 +10,7 @@ export function Layout({ children }: { children: ReactNode }): ReactElement {
     <div className={styles.layout_wrapper} >
       <Head>
         <title>John Barhorst | Web Developer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"></meta>
       </Head>
       <Header />
       {children}
