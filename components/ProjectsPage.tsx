9d2cc9bdea133d0d701d5e71ff12081225347bc1
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-08-29 15:50:45 -0500

    digital garden start

--- components/ProjectsPage.tsx
+++ components/ProjectsPage.tsx
@@ -4,9 +4,9 @@ import style from 'styles/ProjectsPage.module.css';
 export function ProjectsPage():ReactElement {
   return (
     <main className={style.main}>
-      <h2>My Projects</h2>
+      <h2>Digital Garden</h2>
       <section>
-        <p>I have projects. They are good. I swear. Now for a longer sentence for testing purposes,</p>
+        <p>This is a place for small projects or ideas I&apos;ve had.</p>
       </section>
     </main>
   );
