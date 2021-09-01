b405d306a70331ea3b299b26b62180b673a92e86
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-08-31 22:06:44 -0500

    basic set up, button color is bad

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,14 +1,40 @@
 import { useInput } from 'hooks/useInput';
-import { ReactElement } from 'react';
+import { FormEvent, ReactElement, useState } from 'react';
+import style from 'styles/Form.module.css';
 
 export function Form():ReactElement {
-  const [name, resetName] = useInput();
+  const [name] = useInput();
+  const [email] = useInput();
+  const [password] = useInput();
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    setIsLoading(true);
+    setTimeout(() => setIsLoading(false), 3000);
+  }
+
   return (
-    <form>
-      <fieldset>
-        <label htmlFor="name">Name:
-          <input type="text" {...name} />
-        </label>
+    <form onSubmit={handleSubmit} className={style.form} >
+      <fieldset disabled={isLoading}>
+        <div>
+          <label htmlFor="name">Name
+            <input type="text" {...name} />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="email">Email
+            <input type="email" {...email} />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="password">Password
+            <input type="password" {...password} />
+          </label>
+        </div>
+        <div>
+          <button type="submit">Sign Up!</button>
+        </div>
       </fieldset>
     </form>
   );
