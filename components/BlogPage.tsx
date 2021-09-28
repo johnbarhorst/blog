386083566b7cae0f1807006e360eb3e60f9e56d1
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-09-27 20:57:28 -0500

    link to each blog post

--- components/BlogPage.tsx
+++ components/BlogPage.tsx
@@ -1,14 +1,14 @@
-import { PostsList } from 'lib/getPosts';
+import { PostMeta } from 'lib/posts';
 import { ReactElement } from 'react';
 
 
-export function BlogPage({ allPostsData }: { allPostsData: PostsList[]}):ReactElement {
+export function BlogPage({ allPostsData }: { allPostsData: PostMeta[]}):ReactElement {
   return (
     <main>
       <h1>Blog!</h1>
       <section>
         {allPostsData.map(({ id, title }) => (
-          <li key={id}>{title}</li>
+          <li key={id}><a href={`/blog/${id}`}>{title}</a></li>
         ))}
       </section>
     </main>
