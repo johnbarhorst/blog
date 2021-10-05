7f965ac4473d28e5a8fb8ba264efa03c7f934b89
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-10-05 18:58:46 -0500

    react md starter w/ syntax highlighting

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,6 +1,8 @@
 import { getAllPostIds, getPostData, PostMeta } from 'lib/posts';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { ReactElement } from 'react';
+import { Prism as SyntaxHighlighter, SyntaxHighlighterProps } from 'react-syntax-highlighter';
+import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import ReactMarkdown from 'react-markdown';
 
 export default function BlogPost({ postData }: {postData: PostMeta}):ReactElement {
@@ -8,7 +10,27 @@ export default function BlogPost({ postData }: {postData: PostMeta}):ReactElemen
   
   return (
     <main>
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown
+        components={{
+          code({ inline, className, children, ...props }: SyntaxHighlighterProps) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={tomorrow}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          }
+        }}
+      >{content}</ReactMarkdown>
     </main>
   );
 }
@@ -23,7 +45,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   if(typeof(params.id) === 'string') {
-    const postData = await getPostData(params.id);
+    const postData = getPostData(params.id);
     return {
       props: {
         postData
