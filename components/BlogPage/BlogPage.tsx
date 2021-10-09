b8abfd661fbfe49f26efe34317f0d7b475c03b4c
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-10-09 15:18:55 -0500

    re-enabled syntax highlighting

--- components/BlogPage/BlogPage.tsx
+++ components/BlogPage/BlogPage.tsx
@@ -1,11 +1,32 @@
 import { ReactElement } from 'react';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter, SyntaxHighlighterProps } from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import style from './BlogPage.module.css';
 
 export function BlogPage({ content }: {content: string}):ReactElement {
   return (
     <main className={style.main}>
-      <ReactMarkdown skipHtml >{content}</ReactMarkdown>
+      <ReactMarkdown 
+        skipHtml
+        components={{
+          code({ inline, className, children, ...props }: SyntaxHighlighterProps) {
+            const match = /language-(\w+)/.exec(className || '');
+            console.log(props);
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={darcula}
+                language={match[1]}
+                {...props}
+              >{String(children).replace(/\n$/, '')}</SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          }
+        }}
+      >{content}</ReactMarkdown>
     </main>
   );
 }
