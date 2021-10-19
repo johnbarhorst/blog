2892c2c27cbd7d676060fed3f7a06e5d2728f15a
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-10-18 21:42:48 -0500

    commenting out some inner workings

--- components/BlogPage/BlogPage.tsx
+++ components/BlogPage/BlogPage.tsx
@@ -10,7 +10,11 @@ export function BlogPage({ content }: {content: string}):ReactElement {
       <ReactMarkdown 
         skipHtml
         components={{
+          // inline is a either true or undefined. undefined = code block, true = inline backticked code.
+          // className is either undefined, or 'language-somelanguage'
           code({ inline, className, children, ...props }: SyntaxHighlighterProps) {
+            // match returns an array, or null. example of one on a ts code block: 
+            // ['language-ts', 'ts', index: 0, input: 'language-ts', groups: undefined]
             const match = /language-(\w+)/.exec(className || '');
             return !inline && match ? (
               <SyntaxHighlighter
