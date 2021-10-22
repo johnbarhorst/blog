806f648391bd1ece016b58271ff886ddc3ef9570
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-10-21 20:38:50 -0500

    updated email address

--- components/ContactPage/ContactPage.tsx
+++ components/ContactPage/ContactPage.tsx
@@ -8,7 +8,7 @@ export function ContactPage():ReactElement {
       <h1 className='text_center'>Contact!</h1>
       <p>Thanks to the wonders of technology, you can get a hold of me in a variety of ways.</p>
       <ul>
-        <li><EmailLinkIcon>&nbsp;johnbarhorst.dev@gmail.com</EmailLinkIcon></li>
+        <li><EmailLinkIcon>&nbsp;john@johnbarhorst.com</EmailLinkIcon></li>
         <li><PhoneLinkIcon>&nbsp;651-253-2911</PhoneLinkIcon></li>
         <li><GithubLinkIcon>&nbsp;github.com/johnbarhorst</GithubLinkIcon></li>
       </ul>
