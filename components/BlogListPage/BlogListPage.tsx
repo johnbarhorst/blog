9a4f3e9448f4cc03ead59683dc1fda9f234e0abf
johnbarhorst <johnbarhorst.dev@gmail.com>
2021-11-09 21:53:26 -0600

    fun animation on hover

--- components/BlogListPage/BlogListPage.tsx
+++ components/BlogListPage/BlogListPage.tsx
@@ -2,8 +2,9 @@ import Link from 'next/link';
 import { PostMeta } from 'lib/posts';
 import { ReactElement } from 'react';
 import style from './BlogListPage.module.css';
+import { motion } from 'framer-motion';
 
-export function BlogListPage({ allPostsMeta }: { allPostsMeta: PostMeta[]}):ReactElement {
+export function BlogListPage({ allPostsMeta }: { allPostsMeta: PostMeta[] }): ReactElement {
   return (
     <main className={style.main}>
       <section className={style.banner}>
@@ -11,16 +12,21 @@ export function BlogListPage({ allPostsMeta }: { allPostsMeta: PostMeta[]}):Reac
         <p>These are my musings on some recent projects. Each blog is still a work in progress at the moment, but you&apos;re welcome to take a sneak peek!</p>
       </section>
       <section className={style.blog_list} >
+        {/* filtering out the bit I wrote up for BI. Doesn't really belong in the list. Should've made a separate page for it. */}
         {allPostsMeta.filter(({ title }) => title !== 'BI Worldwide').map(({ id, title, description }) => (
           <Link href={`/blog/${id}`} key={id}>
-            <a className={style.blog_card}>
+            <motion.a
+              className={style.blog_card}
+              whileHover={{
+                scale: 1.01
+              }}
+            >
               <h3>{title}</h3>
               <p>{description}</p>
-            </a>
+            </motion.a>
           </Link>
         ))}
       </section>
     </main>
   );
 }
-
